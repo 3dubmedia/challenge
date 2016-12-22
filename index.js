46dfd97b3e7fa6322d3af2598ad81b542b6c1ddb
3dubmedia <jim@fourkitchens.com>
2016-12-22 09:22:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ var express = require('express'),
 var app = express();
 
 var mongoose = require('mongoose');           
-var morgan = require('morgan'); 
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override'); 
 
 mongoose.connect('mongodb://localhost/jobs');
-app.use(morgan('dev')); 
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json()); 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
